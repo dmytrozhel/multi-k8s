6c225a6314aeb0505ef556f2a3d7a30677833c2a
Dmytro Zheleznov <Dmytro.Zheleznov@miratechgroup.com>
2019-08-29 09:25:28 +0300

    made final changes 2

--- server/index.js
+++ server/index.js
@@ -36,6 +36,12 @@ const redisPublisher = redisClient.duplicate();
 // Express route handlers
 
 
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
@@ -48,12 +54,7 @@ app.get('/values/current', async (req, res) => {
   });
 });
 
-app.post('/values', async (req, res) => {
-  const index = req.body.index;
 
-  if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
-  }
 
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
